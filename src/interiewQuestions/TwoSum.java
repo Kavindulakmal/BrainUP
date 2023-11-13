ba442fe61b2dff4319a5e58b387c931a88a74007
Kavindulakmal <avakavindu@gmail.com>
2023-11-14 01:44:40 +0530

    11/14/2023

---
+++ src/interiewQuestions/TwoSum.java
@@ -0,0 +1,32 @@
+//twosum using hashmap
+
+package interiewQuestions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numbers[] = new int[] {2,3,7,4,8};
+		int target =6;
+		int result[] = getTwosum(numbers, target);
+		System.out.print(result[0]+ " " + result[1]);
+
+	}
+	
+	public static int [] getTwosum(int[] numbers, int target) {
+		Map<Integer,Integer> visitedNumbers = new HashMap<>();
+		for(int i=0;i<numbers.length;i++) {
+			int delta = target-numbers[i];
+			if(visitedNumbers.containsKey(delta)) {
+				return new int [] {i,visitedNumbers.get(delta)};
+			}
+			visitedNumbers.put(numbers[i], i);
+		}
+		return new int [] {-1,-1};
+		
+	}
+
+}
