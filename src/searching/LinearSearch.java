aea8094b649a025ead21d8ab456f27c56bd03a00
Kavindulakmal <avakavindu@gmail.com>
2023-11-25 00:25:51 +0530

    11/25/2023

---
+++ src/searching/LinearSearch.java
@@ -0,0 +1,29 @@
+package searching;
+/*
+ * 	Step 1: Traverse the array
+	Step 2: Match the key element with array element
+	Step 3: If key element is found, return the index position of the array element
+	Step 4: If key element is not found, return -1
+ */
+
+public class LinearSearch {
+	
+	public static int linearSearch(int[] arr,int key) {
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==key) {
+				return i;
+			}
+		}
+		return -1;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array [] = {10,20,30,50,70,90};
+		int key =50;
+		System.out.println(key + " found at index : "+linearSearch(array, key));
+
+	}
+
+}
